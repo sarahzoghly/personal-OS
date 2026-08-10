f4c1d838a3d4f0f700ca46b0f9560b4698638835
sarahzoghly <sarahzoghly1223@gmail.com>
2026-08-11 01:49:50 +0300

    Added top bar, logout button, and a warning pop-up

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 const os_button = document.getElementById('enter-os');
+const logout_button = document.getElementById('logout');
+const btn1 = document.getElementById('warning-bt1');
+const btn2 = document.getElementById('warning-bt2');
 let entered_os = false;
+let btn1_clicked = false;
+let btn2_clicked = false;
+let warning = "";
 document.querySelectorAll(".os").forEach(section => section.style.display = "none");
+document.querySelectorAll("#warning").forEach(section => section.style.display = "none")
 document.body.style.overflow = 'hidden';
 
 os_button.addEventListener('click', () => {
-    if (entered_os ){
-        document.querySelectorAll(".welcome").forEach(section => section.style.display = "none");
-        document.querySelectorAll(".os").forEach(section => section.style.display = "grid");
-    }
-    else{
-        entered_os = true;
-    }
+    entered_os = true;
+    os();
 });
 
 const pupils = [
     { el: document.getElementById('pupil-left'), baseX: 90, baseY: 120},
     { el: document.getElementById('pupil-right'), baseX: 145, baseY: 120}
 ];
-const maxDist = 3;
+const maxDist = 10;
 
 document.addEventListener('mousemove', (e) => {
     const catRect = document.getElementById('cat-with-eyes').getBoundingClientRect();
@@ -31,3 +33,81 @@ document.addEventListener('mousemove', (e) => {
         p.el.style.top = (p.baseY + dy) + 'px';
     });
 });
+
+logout_button.addEventListener('click', () => {
+    document.querySelector("#warning-text").innerHTML = `ARE YOU SURE?`;
+    document.querySelector("#warning-bt1").innerHTML = `Yes, logout`;
+    document.querySelector("#warning-bt2").innerHTML = `No`;
+    document.querySelectorAll("#warning").forEach(section => section.style.display = "block");
+    warning = "logout";
+
+});
+
+
+
+btn1.addEventListener('click', () => {
+    btn1_clicked = true;
+    if (warning == "logout"){
+        if (btn1_clicked){
+            welcome();
+            entered_os = false;
+            document.querySelectorAll("#warning").forEach(section => section.style.display = "none");
+        }
+    }
+});
+
+btn2.addEventListener('click', () => {
+    btn2_clicked = true;
+    if (warning == "logout"){
+        if (btn2_clicked){
+            document.querySelectorAll("#warning").forEach(section => section.style.display = "none");
+        }
+    }
+});
+
+
+function welcome(){
+    document.querySelectorAll(".os").forEach(section => section.style.display = "none");
+    document.querySelectorAll(".welcome").forEach(section => section.style.display = "grid"); 
+}
+
+function os(){
+    document.querySelectorAll(".welcome").forEach(section => section.style.display = "none");
+    document.querySelectorAll(".os").forEach(section => section.style.display = "grid");  
+}
+
+dragElement(document.getElementById("warning"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
