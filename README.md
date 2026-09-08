1d02c77f303c4818dd6a29246e81ed75231342dc
sarahzoghly <sarahzoghly1223@gmail.com>
2026-09-09 01:26:05 +0300

    made working weather widget

--- README.md
+++ README.md
@@ -32,7 +32,11 @@ That is my personal OS that can be run on the web.
 
 - <a href= "https://jams.hackclub.com/batch/webOS/part-1/" target="_blank">Hackclub webOS guide</a>
 
-- Icons: <a href= "https://www.flaticon.com/" target="_blank">www.flaticon.com</a>
+- Icons: 
+
+- <a href= "https://www.flaticon.com/" target="_blank">www.flaticon.com</a>
+
+- <a href= "https://giphy.com/" target="_blank">www.giffy.com</a>
 
 (attributes:
 
@@ -45,6 +49,19 @@ That is my personal OS that can be run on the web.
 - <a href="https://www.flaticon.com/free-icons/gaming" title="gaming icons">Gaming icons created by Cuputo - Flaticon</a>
 
 - <a href="https://www.flaticon.com/free-icons/brush" title="brush icons">Brush icons created by Magnific - Flaticon</a>
+
+- <a href="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGF5Mm0yNW13ZWtuM3g4dzdxeWg5eTFuNHZzdW1sdHVtYWcweml6MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CabrEieTaTD5HdRDRM/giphy.gif" title="sun gif">Sun gif created by @LaPageDeCam - Giffy</a>
+
+- <a href="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnR5ZjEwZXlxa2s1cnpmNGRhcXdnYmdxNnoydnk2ZzV6czZqOGlxcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/09MsSFGDZvKOki1kxY/giphy.gif" title="raining sticker">Rain sticker created by @truecuckoo - Giffy</a>
+
+- <a href="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmhpMnZheXI2ZzkybzcyMTkzMzZpZGR2dWxhYmprd216azNscndzMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/LsxB3BtSbnHNhijy6K/giphy.gif" title="cloudy sticker">Clouds sticker created by @catsmartmarketing - Giffy</a>
+
+- <a href="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmdrMnpmcmNobDZrejJ2NDB6Y3hkeWRoYXo3eXF5ZXY1MXc4enN5bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/3ohhwHGift6jATwg4o/giphy.gif" title="lightning gif">Lightning gif created by @bentuber - Giffy</a>
+
+- <a href="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExajlydnk5OHMwZmZ5c2hsNXd6OXpwYXI0aDhlcHc1bzVmeXd5c3J5YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8FvqV3PcJvvB7WFH13/giphy.gif" title="snowing gif">Snowy gif created by @kristyglas - Giffy</a>
+
+- <a href="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWhwNXQ1aGFkc2hzbmx4MThqamR0amM4YnAxdmVsMGs1ZHBvZmVveCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/L05HgB2h6qICDs5Sms/giphy.gif" title="loading sticker">Loading sticker created by @crearecreativita - Giffy</a>
+
 )
 
 
--- main.js
+++ main.js
@@ -421,3 +421,105 @@ function drawing() {
 
 }
 
+function loadWeather() {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        const response = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`
+        );
+
+        const data = await response.json();
+
+        const temp = data.current_weather.temperature;
+
+        // Open-Meteo uses "weathercode" here
+        const code = data.current_weather.weathercode;
+
+        const weatherCodeMap = {
+            0: 'Clear sky',
+            1: 'Mainly clear',
+            2: 'Partly cloudy',
+            3: 'Overcast',
+            45: 'Fog',
+            48: 'Depositing rime fog',
+            51: 'Light drizzle',
+            53: 'Moderate drizzle',
+            55: 'Dense drizzle',
+            56: 'Light freezing drizzle',
+            57: 'Dense freezing drizzle',
+            61: 'Slight rain',
+            63: 'Moderate rain',
+            65: 'Heavy rain',
+            66: 'Light freezing rain',
+            67: 'Heavy freezing rain',
+            71: 'Slight snow fall',
+            73: 'Moderate snow fall',
+            75: 'Heavy snow fall',
+            77: 'Snow grains',
+            80: 'Slight rain showers',
+            81: 'Moderate rain showers',
+            82: 'Violent rain showers',
+            85: 'Slight snow showers',
+            86: 'Heavy snow showers',
+            95: 'Thunderstorm',
+            96: 'Thunderstorm with slight hail',
+            99: 'Thunderstorm with heavy hail'
+        };
+
+        const con = weatherCodeMap[code] || "Unknown";
+
+        document.getElementById('temperature').textContent = `${temp}°C`;
+        document.getElementById('condition').textContent = con;
+
+        const weatherIcon = document.querySelector("#weather-icon img");
+
+        if (code === 95 || code === 96 || code === 99) {
+
+            weatherIcon.src = "images/lightning.gif";
+
+        } else if (
+            code === 71 || code === 73 || code === 75 ||
+            code === 77 || code === 85 || code === 86
+        ) {
+
+            weatherIcon.src = "images/snowy.gif";
+
+        } else if (
+            code === 51 || code === 53 || code === 55 ||
+            code === 56 || code === 57 ||
+            code === 61 || code === 63 || code === 65 ||
+            code === 66 || code === 67 ||
+            code === 80 || code === 81 || code === 82
+        ) {
+
+            weatherIcon.src = "images/raining.webp";
+
+        } else if (code === 0) {
+
+            weatherIcon.src = "images/sunny.gif";
+
+        } else if (code === 1 || code === 2 || code === 3) {
+
+            weatherIcon.src = "images/cloudy.gif";
+
+        } else if (code === 45 || code === 48) {
+
+            weatherIcon.src = "images/foggy.gif";
+
+        } else {
+
+            weatherIcon.src = "images/cloudy.gif";
+        }
+
+    }, (error) => {
+        document.getElementById('temperature').textContent = "something°C";
+        document.getElementById('condition').textContent = "Couldn't get weather";
+    });
+}
+
+
+
+
+loadWeather();
